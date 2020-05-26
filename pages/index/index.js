a16aec3e9405376cb71547fa99c177d807ccfaf1
unknown <unknown@example.com>
2020-05-26 16:11:19 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,56 @@
+
+Page({
+  chooseImageTap: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+  },
+  chooseWxImage: function (type) {
+    var that = this;
+    var imgsPaths = that.data.imgs;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res.tempFilePaths);
+        that.upImgs(res.tempFilePaths[0], 0)
+      }
+    })
+  },
+  upImgs: function (imgurl, index) {
+    var that = this;
+    wx.uploadFile({
+      url: 'https://scaudachuang.xyz:8080/sendImage',//
+      filePath: imgurl,
+      name: 'file',
+      header: {
+        'content-type': 'multipart/form-data'
+      },
+      formData: null,
+      success: function (res) {
+        console.log(res) //接口返回网络路径
+        var data = JSON.parse(res.data)
+        that.data.picPaths.push(data['msg'])
+        that.setData({
+          picPaths: that.data.picPaths
+        })
+        console.log(that.data.picPaths)
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  }
+  
+  
+})
